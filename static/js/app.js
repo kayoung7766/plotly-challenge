6a5744ea5b0236e4503e2249dbfa3634397f4344
Katie <kayoung7766@gmail.com>
2020-09-21 15:00:07 -0500

    can call sample array

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,50 @@
-d3.json("samples.json").then((importedData) => {
-     console.log(importedData);
-    var data = importedData;
 
+
+d3.json("samples.json").then((importedData) => {
+    console.log(importedData);
+    const data = importedData;
     
+
+    let sample=  data.map(row => row.samples);
+    console.log(sample);
+
+    let sampleValues = data.map(row=> row.samples["sample_values"]);
+    console.log(sampleValues)
 });
 
+    // function unpack (rows, index) {
+    //     return rows.map(function(row){
+    //         return row[index];
+    //     });
+    // }
 
-  
+    // let sample_values = unpack(data.names);
+    // console.log(sample_values);
+// });
+    // let trace1 = {
+    //     x : 
+    //     type: "bar"
+    // }
+
+    // let traces = [trace1]
+    // Plotly.newPlot("bar", traces)
+
+    // d3.selectAll("#selDataset").on("change", updatePlotly);
+    // // This function is called when a dropdown menu item is selected
+    // function updatePlotly() {
+    //     // Use D3 to select the dropdown menu
+    //     var dropdownMenu = d3.select("#selDataset");
+    //     console.log(dropdownMenu);
+    //     // Assign the value of the dropdown menu option to a variable
+    //     var dataset = dropdownMenu.property("value");
+    //     console.log(dataset);
+    // }
+
+
+
+
+
+
+ 
+
+
