65897e123efcc52332d984dbe970529716f95082
Katie <kayoung7766@gmail.com>
2020-09-25 20:52:33 -0500

    event handler working

--- static/js/app2.js
+++ static/js/app2.js
@@ -5,32 +5,33 @@ function process() {
     console.log("id", targetId);
 };
 
+function Metadata(sample){
+    d3.json("samples.json").then((data)=> {
+
+        var metadata = data.metadata;
+
+        var firstValueList = metadata.filter(sampleObj => sampleObj.id == sample);
+
+        var metaResult = firstValueList[0];
+
+        var demoCard = d3.select('#sample-metadata');
+        demoCard.html("");
+
+        Object.entries(metaResult).forEach(([key, value]) => {
+            demoCard.append('h5').text(`${key}:${value}`)
+
+        });
+
+    })
 
 
-d3.json("samples.json").then((importedData) => {
-    console.log(importedData);
-    const data = importedData;
 
-    var dropdownMenu = d3.select("#selDataset");
 
-    let sampleId = data.names;
-    console.log(sampleId);
+    }
 
-    // loop for dropdown
-    for (i = 0; i < sampleId.length; i++) {
-        var option = dropdownMenu.append('option');
-        option.attr('value', sampleId[i]);
-        option.text(sampleId[i]);
-    };
 
-    dropdownMenu.on("change", buildCharts);
-    // optionChanged.on("change", buildCharts);
 
-    var targetId = dropdownMenu.property('value');
-    let idx = d3.select("#selDataset").node().value;
-    console.log(idx);
-
-    function buildCharts(idx) { }
+function buildCharts(idx) { 
     d3.json("samples.json").then((data) => {
         var samples = data.samples;
         var resultArray = samples.filter(sampleObj => sampleObj.id == idx);
@@ -41,40 +42,24 @@ d3.json("samples.json").then((importedData) => {
         filteredValues = filteredValues.reverse();
         var otuIds = result.otu_ids;
         var otuLabels = result.otu_labels;
-        //metadata variables
+        // //metadata variables
         var metadata = data.metadata;
         var metaArray = metadata.filter(sampleObj => sampleObj.id == idx);
         var metaResult = metaArray[0];
-        var ethnicity = metaResult.ethnicity;
-        var gender = metaResult.gender;
-        var age = metaResult.age;
-        var bbtype = metaResult.bbtype;
         var wfreq = metaResult.wfreq;
-        var ethnicity = metaResult.ethnicity;
         console.log(sampleValues);
         console.log(filteredValues);
         console.log(otuIds);
         console.log(otuLabels);
-        console.log(ethnicity);
-        console.log(gender);
-        console.log(age);
-        console.log(bbtype);
         console.log(wfreq);
 
-        //IF TIME WITH TUTORING FIGURE OUT WHY THIS DIDN'T WORK
-        // var otuIdAxis = [];
-        // for (i = 0; i < otuIds.length; i++) {
-        //     var otuIdAxis = otuIdAxis.push(`OTU ${otuIds[i]}`);
-        // };
-
-        // console.log(otuIdAxis);
 
 
 
 
         //bar chart
         let trace1 = {
-            x: filteredValues, //ok 
+            x: filteredValues, 
             y: [`OTU ${otuIds[0]}`, `OTU ${otuIds[1]}`, `OTU ${otuIds[2]}`, `OTU ${otuIds[3]}`, `OTU ${otuIds[4]}`,
             `OTU ${otuIds[5]}`, `OTU ${otuIds[6]}`, `OTU ${otuIds[7]}`, `OTU ${otuIds[8]}`, `OTU ${otuIds[9]}`], //not ok
             text: otuLabels,
@@ -118,7 +103,7 @@ d3.json("samples.json").then((importedData) => {
         console.log(wfreq);
 
 
-        //gauge chart (Bonus)
+        //gauge chart (Bonus) https://plotly.com/javascript/gauge-charts/
         var gauge = [
             {
                 domain: { x: [0, 9], y: [0, 9] },
@@ -139,27 +124,46 @@ d3.json("samples.json").then((importedData) => {
 
         var layout3 = { width: 600, height: 500, margin: { t: 0, b: 0 } };
         Plotly.newPlot('gauge', gauge, layout3);
+    });
 
-        //metadata
-
-        var demoCard = d3.select('#sample-metadata');
-        demoCard.html("");
-        Object.entries(metaResult).forEach(([key, value]) => {
-            var demoInfo = demoCard.append('p');
-            demoInfo.text(`${key}:${value}`)
-
-        });
+}
+    
 
 
+function init(){
+    d3.json("samples.json").then((importedData) => {
+        console.log(importedData);
+        const data = importedData;
+
+        var dropdownMenu = d3.select("#selDataset");
+
+        let sampleId = data.names;
+        console.log(sampleId);
+
+        // loop for dropdown
+        for (i = 0; i < sampleId.length; i++) {
+            var option = dropdownMenu.append('option');
+            option.attr('value', sampleId[i]);
+            option.text(sampleId[i]);
+        };
+
+        // Using the first sample from the list to build the initial dashboard
+        var firstSample = sampleId[0];
+        buildCharts(firstSample);
+        Metadata(firstSample);
+    });
+}
+
+function optionChanged(newValue){
+    buildCharts(newValue);
+    Metadata(newValue);
+
+}
+
+init();
 
   
 
-    });
 
-    // function optionChanged(newValue){
-    //     buildCharts(newValue);
 
-    // }
-
-});
 
